00e77e127c8c5f580ac4f4507457161f38e32102
songmag2 <songmag@naver.com>
2018-01-03 17:47:47 +0900

    FastLocation.init

---
+++ FtLH.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<Windows.h>
+
+typedef struct Graph {
+
+
+}G;
+typedef struct vertex {
+	
+}Vertex;
+typedef struct edge {}Edge;
+typedef struct Link {}Link;
